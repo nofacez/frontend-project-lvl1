defe5c471aee541445ab9ac9effbe9d963f4bdd5
nofacez <genizzz@yandex.ru>
2020-12-28 21:00:43 +0300

    linter fix

--- src/calcSolution.js
+++ src/calcSolution.js
@@ -2,13 +2,13 @@ export default (num1, num2, op) => {
   switch (op) {
     case '+':
       return (num1 + num2).toString();
-      break;
+
     case '-':
       return (num1 - num2).toString();
-      break;
+
     case '*':
       return (num1 * num2).toString();
-      break;
+
     default:
       break;
   }
